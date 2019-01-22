7f69be4a224e9109308fc4cde0db33df92fe95a8
Sixto Burga <oswaldoburga@gmail.com>
2019-01-22 08:51:06 -0500

    .

---
+++ proyectoFinal/apps/gitHub/controller.py
@@ -0,0 +1,80 @@
+
+from apps.gitHub.models import Repository
+from datetime import datetime
+import requests
+
+class RepositoryController():
+    def insert(idGitHub,name,description,url,created_at,updated_at,pushed_at):
+        date_format = "%Y-%m-%dT%H:%M:%SZ"
+        repo = Repository.objects.create(idGitHub = int(idGitHub))
+        repo.name = name
+        repo.description = description
+        repo.url = url
+        repo.created_at = datetime.strptime(created_at, date_format)
+        repo.updated_at = datetime.strptime(updated_at, date_format)
+        repo.pushed_at = datetime.strptime(pushed_at, date_format)
+        repo.save()
+
+    def update(id,name,description,url,updated_at,pushed_at):
+        date_format = "%Y-%m-%dT%H:%M:%SZ"
+        repo = Repository.objects.get(id=id)
+        repo.name = name
+        repo.description = description
+        repo.url = url
+        repo.updated_at = datetime.strptime(updated_at, date_format)
+        repo.pushed_at = datetime.strptime(pushed_at, date_format)
+        repo.save()
+
+    def validate(idGitHub,updated_at,pushed_at):
+        # return 0: No existe el repositorio
+        # return 1: Existe pero no hubo cambios
+        # return 2: Existe y si hubo cambios
+        date_format = "%Y-%m-%dT%H:%M:%SZ"
+        res=0
+        id=0
+        repo = Repository.objects.filter(idGitHub=int(idGitHub))
+        if repo:
+            mod_updated_at=datetime.strptime(updated_at, date_format)
+            print(mod_updated_at)
+            print(repo[0].updated_at)
+
+            if mod_updated_at!=repo[0].updated_at:
+                res=2
+                id=repo[0].id
+            else:
+                res=1
+        return res,id
+
+    def sync(self):
+        r = requests.get('https://api.github.com/orgs/githubtraining/repos', headers={'Authorization': 'token 5dc2849f045ffe8d47769ff825db547ed3c261b5'})
+        data = r.json()
+
+        if 'message' in data:
+            print('Error API')
+        else:
+            for obj in data:
+                name= obj['name']
+                idGitHub=obj['id']
+                description= obj['description']
+                url= obj['url']
+                created_at= obj['created_at']
+                updated_at= obj['updated_at']
+                pushed_at= obj['pushed_at']
+
+                print('idGitHub')
+                print(idGitHub)
+
+               
+                resultValidate, id=RepositoryController.validate(idGitHub,updated_at,pushed_at)
+                print(resultValidate)
+
+                if resultValidate > 0:
+                    print('ya existe')
+                    if resultValidate==2: # Existe y si hubo cambios
+                        print('update')
+                        print(id)
+                        RepositoryController.update(id,name,description,url,updated_at,pushed_at)
+                else:
+                    print('nuevo')
+                    RepositoryController.insert(idGitHub,"demo",description,url,created_at,updated_at,pushed_at)
+                break
--- proyectoFinal/apps/gitHub/models.py
+++ proyectoFinal/apps/gitHub/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Repository(models.Model):
+    idGitHub = models.IntegerField()
+    name = models.CharField(max_length=200 ,null=True )
+    description = models.CharField(max_length=200,null=True )
+    url = models.URLField(null=True )
+    created_at = models.DateField(null=True )
+    updated_at = models.DateField(null=True )
+    pushed_at = models.DateField(null=True )
